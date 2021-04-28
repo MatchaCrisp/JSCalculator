f388ab1cb42127b32f77b13484e97d4f20c82e09
MatchaCrisp <junyanbill@hotmail.com>
2021-04-27 23:32:36 -0400

    v0.1

---
+++ changelog.md
@@ -0,0 +1,19 @@
+# v0.1 Brainstorm
+## new
+ * created app
+ * installed gh-pages/mathjs
+
+## to do
+ * Basic arithmetic +/-/*//
+ * Sqrt/^2
+ * negative numbers
+ * AC/Del
+ * integer operations
+ * decimal operations
+ * process calculation on enter/on new operator
+ * scientific notation toggle
+ * ON/OFF switch
+ 
+ * calculation history/undo
+ * on the fly number edit
+ * history change
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const Pad=props=>{
+
+  return (
+    <button className="userPad"
+    
+    />
+  )
+}
+
+const App=()=>{
+  return (
+    <div id="app">
+      <Calc />
+    </div>
+  )
+}
+
+const Calc=props=>{
+  return (
+    <div id="calc">
+      <Ctrl />
+      <CalcPad />
+    </div>
+  )
+}
+
+const Ctrl=props=>{
+  return (
+    <div id="ctrl">
+
+    </div>
+  )
+}
+
+const CalcPad=props=>{
+  return (
+    <div id="calcPad">
+
+    </div>
+  )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
