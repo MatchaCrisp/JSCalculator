938851a8d68d770165276eda677a2ffc3ff5cbf5
MatchaCrisp <junyanbill@hotmail.com>
2021-04-28 14:10:18 -0400

    skeletal layout/interactibles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import './index.scss';
 
 const Pad=props=>{
-
+  const dispTxt=(props.desc==="pow")?(<p>x<sup>2</sup></p>):(<p>props.desc</p>);
+  useEffect(()=>{
+    if (props.key==="") return;
+    document.addEventListener('keydown',handleKey);
+    return ()=>{document.removeEventListener('keydown',handleKey)};
+  });
+  const handleKey=e=>{
+    if (e.keyCode===props.key) 
+      props.handleEvent();
+  }
+  const handleClick=()=>{
+    props.handleEvent();
+  }
   return (
-    <button className="userPad"
-    
-    />
+    <div className={props.className}
+            id={props.id}
+            onClick={handleClick}>
+              {dispTxt}
+            </div>
   )
 }
 
 const App=()=>{
+  
   return (
     <div id="app">
       <Calc />
@@ -20,26 +36,50 @@ const App=()=>{
 }
 
 const Calc=props=>{
+  const [curr,setCurr]=useState('');
+  const handleEvent=e=>{
+    setCurr(e);
+  }
   return (
     <div id="calc">
-      <Ctrl />
-      <CalcPad />
+      <Display curr={curr}/>
+      <CalcPad handleEvent={handleEvent}/>
     </div>
   )
 }
 
-const Ctrl=props=>{
+const Display=props=>{
   return (
-    <div id="ctrl">
-
+    <div id="display">
+      {props.curr}
     </div>
   )
 }
 
 const CalcPad=props=>{
+  const [url]=useState('%PUBLIC_URL%/button.json');
+  const [buttons,setButtons]=useState([]);
+  const renderButton=button=><Pad
+                                className={button.class}
+                                id={button.id}
+                                key={button.keyCode}
+                                desc={button.desc}
+                                handleEvent={props.handleEvent} />
+  useEffect(()=>{
+    if (!url) return;
+
+    const fetchData=async()=>{
+      const response=await fetch(url);
+      const data=await response.json();
+      setButtons(data.buttList);
+    }
+    fetchData().catch(console.log);
+  },[url]);
+
+  const jsx=buttons.map(button=>renderButton(button));
   return (
     <div id="calcPad">
-
+      {jsx}
     </div>
   )
 }
